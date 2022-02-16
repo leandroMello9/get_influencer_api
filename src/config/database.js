fc774b2fddd59f005ea4f30c3daad3b4b1266e67
Leandro De Melo De Oliveira <oliveiraleandro@brq.com>
2022-02-16 11:51:26 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'admin',
-    database: 'fitvlogger',
+    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USER_NAME || 'root',
+    password: process.env.DB_PASSWORD || 'admin',
+    database: process.env.DB_NAME || 'fitvlogger',
     port: 3306,
     define: {
       timestamps: true,
